806cc45418eb5da7f66a1c8554ee998a3ace1e7f
Sanjeet Kang <kang.sanjeet1401@gmail.com>
2020-05-27 21:30:55 -0700

    finished styling the app

--- client/src/Components/ImageList.js
+++ client/src/Components/ImageList.js
@@ -1,35 +1,33 @@
 import React,{useState} from 'react';
 import ImageListItem from './ImageListItem'
 import classNames from 'classnames'
+import {Button} from 'react-bootstrap'
 import './ImageList.scss'
-import axios from 'axios'
 
 
 
 export default function ImageList({images, accessKey}) {
-  const [query,setQuery] = useState("")
-  // const buttonClass = classNames("button", {
-  //   "button--confirm": props.confirm,
-  //   "button--danger": props.danger
-  // });
+  const [view,setView] = useState("list")
+  const buttonClass = classNames({
+    "list": view === "list",
+    "gallery": view === "gallery"
+  });
+
+  //Opens a link in a new tab to download the picture
   const onDownload = (id) => {
     window.open(`https://unsplash.com/photos/${id}/download?force=true`, "_blank");
-    axios.get(`https://unsplash.com/photos/${id}/download?force=true`, {
-            params: { 
-              client_id: accessKey
-            }
-        })
-        .then((res) => {
-          console.log(res)
-        })
-        .catch(() => {
-          console.log("Rate Limit Exceeded")
-        })
   }
 
   return (
-    <div className="list">
-      {images.map((image,key) => <ImageListItem image={image} key={key} onDownload={() => onDownload(image.id)}/>)}
+    <div className="holder">
+      <div className="view">
+        <h2>View by: </h2>
+        <Button variant={view === "list" ? "primary" : "outline-dark"} onClick={() => setView("list")}>List</Button>
+        <Button variant={view === "gallery" ? "primary" : "outline-dark"} onClick={() => setView("gallery")}>Gallery</Button>
+      </div>
+      <div className={buttonClass}>
+        {images.map((image,key) => <ImageListItem image={image} key={key} onDownload={() => onDownload(image.id)}/>)}
+      </div>
     </div>
   );
 }
